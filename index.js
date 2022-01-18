c16a676202ab412aaf64bff412b19b921cc0f0d8
Shahriar Islam <theshahriarislam@gmail.com>
2022-01-18 20:10:28 +0600

    get datafrom body using buffer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // dependencies
 const http = require('http');
+const url = require('url');
+const {StringDecoder} = require('string_decoder');
 
 // app object - model scaffolding
 const app = {};
@@ -14,12 +16,34 @@ app.createServer = () => {
     const server = http.createServer(app.handleReqRes);
     server.listen(app.config.port, ()=> {
         console.log('Listening to', app.config.port);
-    })
+    });
 }
 
 // handle Request Response
 app.handleReqRes = (req, res)=> {
-    res.end('Hello world');
+    // request handling
+    // get the url and parse it
+    const parsedUrl = url.parse(req.url, true);
+    const path = parsedUrl.path;
+    const trimedPath = path.replace(/^\/+|\/+$/g, '');
+    
+    const method = req.method.toLowerCase();
+    const queryString = parsedUrl.query;
+    const headers = req.headers;
+    
+    const decoder = new StringDecoder('utf-8');
+    const realData = '';
+
+    req.on('data', (buffer)=> {
+        realData += decoder.write(buffer);
+    })
+
+    req.on('end', ()=> {
+        realData += decoder.end();
+    })
+
+    // response
+    res.end('Hello prog');
 };
 
 // start seever
